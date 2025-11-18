03416f18900e5c294332fac9375e69bb9120822a
Julius <julius0216@outlook.com>
2025-11-18 09:57:07 -0800

    rootRoute helper

--- test/_utils-browser.tsx
+++ test/_utils-browser.tsx
@@ -2,12 +2,19 @@ import { ConvexQueryClient } from "@convex-dev/react-query";
 import { QueryClient } from "@tanstack/react-query";
 import { ConvexReactSessionClient } from "convex-helpers/react/sessions";
 import { it, inject } from "vitest";
+import { AuthKitProvider } from "@workos/authkit-tanstack-react-start/client";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { createRootRoute, Outlet } from "@tanstack/react-router";
+import { ConvexProvider } from "convex/react";
+import { AnyRootRoute } from "@tanstack/react-router";
+import { SessionId } from "convex-helpers/server/sessions";
 
 export const test = it.extend<{
   convexBackendUrl: string;
   convexClient: ConvexReactSessionClient;
   convexQueryClient: ConvexQueryClient;
   queryClient: QueryClient;
+  rootRoute: AnyRootRoute;
 }>({
   convexBackendUrl: async ({}, use) => {
     const convexBackendUrl = inject("convexBackendUrl");
@@ -16,6 +23,7 @@ export const test = it.extend<{
   },
   convexClient: async ({ convexBackendUrl }, use) => {
     const convexClient = new ConvexReactSessionClient(convexBackendUrl);
+    convexClient.setSessionId(crypto.randomUUID() as SessionId);
     await use(convexClient);
   },
   convexQueryClient: async ({ convexClient }, use) => {
@@ -33,5 +41,26 @@ export const test = it.extend<{
     });
     convexQueryClient.connect(queryClient);
     await use(queryClient);
+  },
+  rootRoute: async ({ convexClient, queryClient }, use) => {
+    function RootComponent() {
+      return (
+        <AuthKitProvider>
+          <ConvexProvider client={convexClient}>
+            <QueryClientProvider client={queryClient}>
+              <Outlet />
+            </QueryClientProvider>
+          </ConvexProvider>
+        </AuthKitProvider>
+      );
+    }
+
+    const rootRoute = createRootRoute({
+      beforeLoad: async () => {
+        // ...
+      },
+      component: RootComponent,
+    });
+    await use(rootRoute);
   },
 });
--- test/sample.browser.test.tsx
+++ test/sample.browser.test.tsx
@@ -1,35 +1,47 @@
-/// <reference types="vite/client" />
-
-import * as React from "react";
 import { expect } from "vitest";
 import { AddNumberButton, Numbers } from "../src/component/numbers";
 import { render } from "vitest-browser-react";
-import { ConvexProvider } from "convex/react";
-import { QueryClientProvider } from "@tanstack/react-query";
 import { test } from "./_utils-browser";
 import { userEvent } from "vitest/browser";
+import {
+  createRoute,
+  createRouter,
+  RouterProvider,
+} from "@tanstack/react-router";
 
-test("render numbers", async ({ convexClient, queryClient }) => {
-  const $ = await render(
-    <QueryClientProvider client={queryClient}>
-      <ConvexProvider client={convexClient}>
+test("render numbers", async ({ rootRoute }) => {
+  const index = createRoute({
+    path: "/",
+    getParentRoute: () => rootRoute,
+    component: () => (
+      <>
         <Numbers />
-      </ConvexProvider>
-    </QueryClientProvider>,
-  );
+      </>
+    ),
+  });
+  const routeTree = rootRoute.addChildren([index]);
+  const router = createRouter({ routeTree });
+  await router.load();
+  const $ = await render(<RouterProvider router={router} />);
 
   await expect.element($.getByTestId("numbers")).toBeInTheDocument();
 });
 
-test("add number", async ({ convexClient, queryClient }) => {
-  const $ = await render(
-    <QueryClientProvider client={queryClient}>
-      <ConvexProvider client={convexClient}>
+test("add number", async ({ rootRoute }) => {
+  const index = createRoute({
+    path: "/",
+    getParentRoute: () => rootRoute,
+    component: () => (
+      <>
         <Numbers />
         <AddNumberButton />
-      </ConvexProvider>
-    </QueryClientProvider>,
-  );
+      </>
+    ),
+  });
+  const routeTree = rootRoute.addChildren([index]);
+  const router = createRouter({ routeTree });
+  await router.load();
+  const $ = await render(<RouterProvider router={router} />);
 
   await userEvent.click($.getByText("Add Number"));
 
